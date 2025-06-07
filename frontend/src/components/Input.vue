e5764af701b4547440e8cc8abd51ba1ce15f6556
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2025-06-07 22:22:15 +0200

    wip: input box

---
+++ frontend/src/components/Input.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+    import type { Ref } from 'vue'
+    const content: Ref<string> = ref('')
+</script> 
+
+<template>
+    <div class="input-container">
+        <input type="text" v-model="content" placeholder="" />
+        <label>Input</label>
+    </div>
+</template>
+
+  
+<style scoped>
+  .input-container {
+    position: relative;
+    width: 100%;
+  }
+  
+  input {
+    border: none;
+    appearance: none;
+    outline: none;
+      
+    box-sizing: content-box;
+    width: 100%;
+    padding: var(--input-padding-top) var(--input-padding-horizontal) var(--input-padding-bottom);
+    border-radius: var(--border-radius);
+    background-color: var(--color-neutral-200);
+    
+    font-family: Author;
+    font-size: 18px;
+    font-weight: var(--font-weight-normal);
+    color: var(--color-neutral-900);
+  }
+  
+  input::placeholder {
+    color: transparent; /* remove placeholder */
+  }
+  
+  label {
+    position: absolute;
+    top: 50%;
+    margin: 0;
+    padding: 0 var(--input-padding-horizontal);
+    
+    color: #aaa;
+    background: var(--color-neutral-200);
+    
+    font-family: Author;
+    font-size: 18px;
+    color: var(--color-neutral-900);
+    font-weight: var(--font-weight-normal);
+
+    transform: translateY(-50%);
+    pointer-events: none;
+    transition: 0.2s ease all;
+  }
+  
+  /* Put label above if input is focused or contains content */
+  input:focus + label,
+  input:not(:placeholder-shown) + label {
+    top: 14px;
+    font-family: Author;
+    font-size: 14px;
+    font-weight: var(--font-weight-normal);
+    color: var(--color-neutral-400);
+  }
+</style>
+  
--- frontend/src/views/Example.vue
+++ frontend/src/views/Example.vue
@@ -1,79 +1,33 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import Button from '../components/Button.vue'
-
-const handleClick = () => {
-  console.log('Button clicked!')
-}
+import Input from '../components/Input.vue';
 </script>
 
 <template>
-  <main>
-    <h1>CampusCar Styles</h1>
-
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    
-    <h2>Button Examples</h2>
-    
-    <div class="button-group">
-      <h3>Primary Buttons</h3>
-      <Button @click="handleClick">Default Primary</Button>
-      <Button color="danger" @click="handleClick">Danger Primary</Button>
-      <Button disabled>Disabled Primary</Button>
+  <div class="view-container">
+    <h1>Beispiel</h1>
+    <h2>Daten Eintragen 1</h2>
+    <div class="container">
+      <Input />
+      <Input />
     </div>
-
-    <div class="button-group">
-      <h3>Secondary Buttons</h3>
-      <Button variant="secondary" @click="handleClick">Default Secondary</Button>
-      <Button variant="secondary" color="danger" @click="handleClick">Danger Secondary</Button>
-      <Button variant="secondary" disabled>Disabled Secondary</Button>
+    <h2>Daten Eintragen 2</h2>
+    <div class="container">
+      <Input />
+      <Input />
+      <Input />
     </div>
-
-    <div class="button-group">
-      <h3>Link Buttons</h3>
-      <Button to="/about">Link to About</Button>
-      <Button variant="secondary" to="/contact">Link to Contact</Button>
-    </div>
-
-    <h3>Text Examples</h3>
-    <p>Normal text</p>
-    <p class="text-bold">Bold text</p>
-    <p class="text-s">Small text</p>
-    <p class="text-xs">Extra small text</p>
-    <p class="text-primary">Primary text</p>
-    <p class="text-info">Info text</p>
-    <p class="text-warning">Warning text</p>
-    <p class="text-danger">Danger text</p>
-
-  </main>
+  </div>
 </template>
 
 <style scoped>
-main {
-  padding: 2rem;
-  text-align: flex-start;
-  align-items: flex-start;
+.view-container h2:first-of-type {
+  margin-top: 0;
 }
 
-.button-demo {
-  margin-top: 2rem;
-}
-
-.button-group {
-  margin: 2rem 0;
+.container {
+  width: 100%;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  gap: 1rem;
-}
-
-h2 {
-  margin-bottom: 1rem;
-}
-
-h3 {
-  margin-bottom: 0.5rem;
+  gap: var(--horizontal-gap)
 }
 </style>
