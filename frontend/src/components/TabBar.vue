c9469744d4125e3af75108afcd8621a835fbef18
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2025-06-25 22:10:29 +0200

    feat: responsive-ish design

--- frontend/src/components/TabBar.vue
+++ frontend/src/components/TabBar.vue
@@ -16,32 +16,54 @@ const isActive = (path: string): boolean => route.path === path
 
 <template>
   <nav>
-    <RouterLink
-      v-for="item in items"
-      :key="item.label"
-      :to="item.to"
-      class="text-xs text-bold text-neutral-300"
-      :class="{ 'text-neutral-900': isActive(item.to) }"
-    >
-      <component :is="item.icon" class="icon-xl" />
-      {{ item.label }}
-    </RouterLink>
+    <div class="icon-container">
+      <RouterLink
+        v-for="item in items"
+        :key="item.label"
+        :to="item.to"
+        class="text-xs text-bold text-neutral-300"
+        :class="{ 'text-neutral-900': isActive(item.to) }"
+      >
+        <component :is="item.icon" class="icon-xl" />
+        {{ item.label }}
+      </RouterLink>
+    </div>
   </nav>
 </template>
 
 <style scoped>
 nav {
   position: fixed;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   left: 0;
   right: 0;
   bottom: 0;   
-  display: flex;
-  justify-content: space-around;
-  flex-direction: row;
-  min-height: var(--nav-min-height);
-  width: 100%;
   box-shadow: 0 -1px 10px var(--color-neutral-900-transparent);
   background-color: var(--color-neutral-100);
+}
+
+.icon-container {
+  width: 100%;
+  min-height: var(--nav-min-height);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: row;
+}
+
+@media (min-width: 768px) {
+  .icon-container {
+    max-width: var(--tablet-content-max-width);
+  }
+}
+
+@media (min-width: 1200px) {
+  .icon-container {
+    max-width: var(--desktop-content-max-width);
+  }
 }
 
 a {
