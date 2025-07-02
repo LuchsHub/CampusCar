67bb2971119f7eb9e245c6a8861a4f69e6484f93
Laurin Krüger <krueger@th-brandenburg.de>
2025-07-02 22:30:52 +0200

    structure type of user fixed

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -1,16 +1,8 @@
 // services/user.ts
-import axios from 'axios'
-
-export interface CurrentUser {
-  id: number
-  user_name: string
-  first_name: string
-  last_name: string
-  email: string
-  avatar_url?: string
-}
+import api from './api'
+import type { CurrentUser } from '@/types/User'
 
 export const fetchCurrentUser = async (): Promise<CurrentUser> => {
-  const response = await axios.get('/api/v1/users/me')
+  const response = await api.get('users/me')
   return response.data
 }
--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -20,3 +20,12 @@ export interface UserUpdate extends Record<string, string | LocationCreate | und
   email?: string
   location?: LocationCreate 
 }
+
+export interface CurrentUser {
+  id: number
+  user_name: string
+  first_name: string
+  last_name: string
+  email: string
+  avatar_url?: string
+}
