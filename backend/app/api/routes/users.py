c823e41860762b8db59ded3eb4f1adef4d6d513d
LuchsHub <robinwagner1910.rw@gmail.com>
2025-07-18 01:52:16 +0200

    Remove privilege checks for retrieving user by ID

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -224,13 +224,6 @@ def read_user_by_id(
     Get a specific user by id.
     """
     user = session.get(User, user_id)
-    if user == current_user:
-        return user
-    if not current_user.is_superuser:
-        raise HTTPException(
-            status_code=403,
-            detail="The user doesn't have enough privileges",
-        )
     return user
 
 
